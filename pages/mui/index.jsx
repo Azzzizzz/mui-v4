ec0bfc28738d323885446925f47105edc80bd055
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-28 20:46:20 +0530

    Initial Commit

---
+++ pages/mui/index.jsx
@@ -0,0 +1,23 @@
+import {Typography, Button} from "@material-ui/core";
+
+const mui = () => {
+   return (
+      <>
+         <Typography variant="h1" color="primary" align="center">
+            h1 Heading
+         </Typography>
+         <Typography variant="h2">h2 Heading</Typography>
+      
+         <p>Lorem, ipsum dolor.</p>
+         <button>ok</button>
+         <Button>submit</Button>
+
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, odit deserunt? Voluptatum consectetur esse labore exercitationem accusantium vitae, porro, excepturi ipsa, reiciendis enim totam quod recusandae tempore aspernatur fuga? Eius!</p>
+         <input type="text"/>
+         <div></div>
+      </>
+   );
+};
+
+
+export default mui;
