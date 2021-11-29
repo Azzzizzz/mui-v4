58c7b196d04479f6550f35d4ec1c9256442bd779
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-29 07:36:50 +0530

    makeStyles

--- pages/mui/index.jsx
+++ pages/mui/index.jsx
@@ -4,19 +4,37 @@ import {
   ButtonGroup,
   Container,
   IconButton,
+  makeStyles,
 } from "@material-ui/core";
 
 import AcUnitOutlinedIcon from "@material-ui/icons/AcUnitOutlined";
 import SendIcon from "@material-ui/icons/Send";
 
+const useStyles = makeStyles({
+  btn:{
+    fontSize: 60,
+    backgroundColor: 'royalblue',
+    "&:hover":{
+      backgroundColor:'violet'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom:20,
+  }
+
+})
+
 const Create = () => {
+  const classes = useStyles()
   return (
     <>
       <Container>
-        <Typography variant="h6" component="h2" gutterBottom>
+        <Typography className={classes.title} variant="h6" component="h2" gutterBottom>
           Create a New Note
         </Typography>
         <Button
+          className={classes.btn}
           onClick={() => console.log("btn clicked")}
           type="submit"
           color="secondary"
@@ -25,8 +43,9 @@ const Create = () => {
         >
           Submit
         </Button>
-        <AcUnitOutlinedIcon />
-        <AcUnitOutlinedIcon color="secondary" fontSize="large" />   </Container>
+        <AcUnitOutlinedIcon   />
+        <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      </Container>
       
     </>
   );
