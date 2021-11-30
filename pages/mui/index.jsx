741044d7b1139bd8a55ab214c78803637eab997e
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-30 09:39:54 +0530

    TextField, useState & error handling

--- pages/mui/index.jsx
+++ pages/mui/index.jsx
@@ -5,18 +5,46 @@ import {
   Container,
   IconButton,
   makeStyles,
+  TextField,
 } from "@material-ui/core";
 
-import AcUnitOutlinedIcon from "@material-ui/icons/AcUnitOutlined";
+import { useState } from "react";
+
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 
 const useStyles = makeStyles({
 
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+
+
+  }
 
 })
 
 const Create = () => {
   const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == '')
+      setTitleError(true)
+    if (details == '')
+      setDetailsError(true)
+    if (title, details)
+      console.log(title, details);
+
+  }
+
   return (
     <>
 
@@ -24,15 +52,44 @@ const Create = () => {
         <Typography variant="h6" component="h2" gutterBottom>
           Create a New Note
         </Typography>
-        <Button
-          onClick={() => console.log("btn clicked")}
-          type="submit"
-          color="secondary"
-          variant="contained"
-          endIcon={<KeyboardArrowRightIcon />}
-        >
-          SUBMIT
-        </Button>
+
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+
+          <TextField
+            onChange={(e) => setTitle(e.target.value)}
+            className={classes.field}
+            variant="filled"
+            label="Note Title"
+            color="secondary"
+            fullWidth
+            required
+            error={titleError}
+          />
+          <TextField
+            onChange={(e) => setDetails(e.target.value)}
+
+            className={classes.field}
+            label="Details"
+            variant="filled"
+            color="secondary"
+            multiline
+            rows={4}
+            fullWidth
+            required
+            error={detailsError}
+          />
+
+
+          <Button
+            type="submit"
+            color="secondary"
+            variant="contained"
+            endIcon={<KeyboardArrowRightIcon />}
+          >
+            SUBMIT
+          </Button>
+        </form>
+
 
 
 
