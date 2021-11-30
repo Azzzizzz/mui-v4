23427f9e7e8d31b507b0e473e96b0f1d3c32eb73
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-30 08:16:50 +0530

    CreateTheme & ThemeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import { tomato  } from '@material-ui/core/colors'
+
+const theme = createTheme({
+  palette:{
+    primary:{
+      main:'#fefefe'
+    },
+    secondary: tomato
+  },
+  typography:{
+    fontFamily: 'Quicksand',
+    fontWeightLight:400,
+    fontWeightRegular:500,
+    fontWeightMedium:600,
+    fontWeightBold:700,
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+         <Component {...pageProps} />
+        
+    </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
--- pages/mui/index.jsx
+++ pages/mui/index.jsx
@@ -8,20 +8,10 @@ import {
 } from "@material-ui/core";
 
 import AcUnitOutlinedIcon from "@material-ui/icons/AcUnitOutlined";
-import SendIcon from "@material-ui/icons/Send";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 
 const useStyles = makeStyles({
-  btn:{
-    fontSize: 60,
-    backgroundColor: 'royalblue',
-    "&:hover":{
-      backgroundColor:'violet'
-    }
-  },
-  title: {
-    textDecoration: 'underline',
-    marginBottom:20,
-  }
+
 
 })
 
@@ -29,24 +19,25 @@ const Create = () => {
   const classes = useStyles()
   return (
     <>
+
       <Container>
-        <Typography className={classes.title} variant="h6" component="h2" gutterBottom>
+        <Typography variant="h6" component="h2" gutterBottom>
           Create a New Note
         </Typography>
         <Button
-          className={classes.btn}
           onClick={() => console.log("btn clicked")}
           type="submit"
           color="secondary"
           variant="contained"
-          endIcon={<SendIcon />}
+          endIcon={<KeyboardArrowRightIcon />}
         >
-          Submit
+          SUBMIT
         </Button>
-        <AcUnitOutlinedIcon   />
-        <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+
+
+
       </Container>
-      
+
     </>
   );
 };
